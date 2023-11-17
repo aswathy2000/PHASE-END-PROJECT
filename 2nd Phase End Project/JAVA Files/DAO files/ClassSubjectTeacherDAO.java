15ac2dd88b59c9357b15b76a961184c1d83072d1
aswathy2000 <131588135+aswathy2000@users.noreply.github.com>
2023-11-17 17:09:35 +0530

    Changes committed

--- 2nd Phase End Project/JAVA Files/DAO files/ClassSubjectTeacherDAO.java
+++ 2nd Phase End Project/JAVA Files/DAO files/ClassSubjectTeacherDAO.java
@@ -15,54 +15,6 @@ import com.project.pojo.Student;
 
 public class ClassSubjectTeacherDAO 
 {
-	
-//	 Session sessionHb = session.openSession();
-//    String sql = "select stud.firstname,stud.roll,sub.subjectname,cls.classname,exam.exam_name,exam.from_time,exam.to_time,exam.exam_date from Student stud,Subject sub,Classs cls,Exam exam where exam.classs=cls.id and exam.subject=sub.id and sub.classs=cls.id and stud.classs=cls.id and stud.roll= :roll";
-//    SQLQuery query = sessionHb.createSQLQuery(sql);
-//    query.setParameter("roll", a);
-//    query.setResultTransformer(Criteria.ALIAS_TO_ENTITY_MAP);
-//    return query.list();
-	
-	
-	
-	
-	//String query = "select c.classId, c.className, t.teacherName, s.subjectName from Classes c join Teacher t ON c.teacherId = t.teacherId Join Subject s ON c.subjectId = s.subjectId  where c.className =:c.cName order by c.classId";
-	
-
-//	Query query = session.createQuery(query);
-//	List<Classes> listStudents = query.list();
-	
-	
-//	public List<Object[]> viewReport(String classname)
-//	{
-//				
-//		Session session = DbUtil.dbConn();
-//		Transaction transaction = session.beginTransaction();
-//		
-//		Query query= session.createQuery("select c.classId, c.className, t.teacherName, s.subjectName "
-//				+ "from Classes c join Teacher t ON c.teacherId = t.teacherId "
-//				+ "Join Subject s ON c.subjectId = s.subjectId  "
-//				+ "where c.className =:c.cName order by c.classId");
-//		
-//		query.setParameter("cName", classname);
-//		
-//		List<Object[]> classObj = query.list();
-//			
-//		
-//		transaction.commit();
-//		session.close();
-//	}
-		
-//		Object[] obj;
-//		for(obj : classObj)
-//		{
-//			Classes classOb = new Classes();
-//			classOb.setClassId(Integer.parseInt((obj[0].toString())));
-//			classOb.setClassName(obj[1].toString());
-//			classOb.getSubject().setSubjectName(obj[2].toString());
-//			classOb.getTeacher().setTeacherName(obj[3].toString());
-//		}
-		
 		 public List<Classes> getClassesByClassName(String className) {
 		       try {
 		           Session session = DbUtil.dbConn();
